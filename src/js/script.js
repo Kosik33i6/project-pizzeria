f057ea4de03dba308d56e0f49a0081cf1a8930aa
Kos Krzysztof <krzysztof.kos33@gmail.com>
2021-06-19 16:27:10 +0200

    Add method initAccordion for class Product

--- src/js/script.js
+++ src/js/script.js
@@ -54,27 +54,57 @@
 
   class Product {
     constructor(id, data) {
+      //  * create const thisProduct
       const thisProduct = this;
       thisProduct.id = id;
       thisProduct.data = data;
 
       // * render after creating inctance
       thisProduct.renderInMenu();
+      thisProduct.initAccordion();
     }
     renderInMenu() {
+      //  * create const thisProduct
       const thisProduct = this;
 
       //  * generate HTMLL based on template
       const generatedHTML = templates.menuProduct(thisProduct.data);
+      // console.log(thisProduct.data);
 
       // * create element using utils.createElementFromHTML
       thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+      // console.log(thisProduct.element);
 
       //  * find menu container
       const menuContainer = document.querySelector(select.containerOf.menu);
 
       //  * add element to menu
       menuContainer.appendChild(thisProduct.element);
+    }
+    initAccordion() {
+      //  TODO: create const thisProduct
+      const thisProduct = this;
+
+      // TODO:  find the clickable trigger (the element that should react to clicking)
+      // ? QUESTION for mentor: where did  thisProduct.element come from?
+      const productElement = thisProduct.element;
+      const clickableTrigger = productElement.querySelector(select.menuProduct.clickable);
+
+      // TODO: START: add event listener to clickable trigger on event click
+      clickableTrigger.addEventListener('click', function(event) {
+        // TODO: prevent default action for event
+        event.preventDefault();
+
+        // TODO: find active product (product that has active class)
+        const activeProduct = document.querySelector(select.all.menuProductsActive);
+
+        // TODO: if there is active product and it's not thisProduct.element, remove class active from it
+        if(activeProduct !== null && activeProduct.classList.contains(classNames.menuProduct.wrapperActive) && activeProduct !== productElement) {
+          activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
+        }
+        // TODO: toggle active class on thisProduct.element
+        productElement.classList.toggle(classNames.menuProduct.wrapperActive);
+      });
     }
   }
 
@@ -85,7 +115,7 @@
 
       for(let productData in thisApp.data.products) {
         new Product(productData, thisApp.data.products[productData]);
-        console.log(new Product(productData, thisApp.data.products[productData]));
+        // console.log(new Product(productData, thisApp.data.products[productData]));
       }
     },
 
@@ -99,8 +129,8 @@
       const thisApp = this;
       // console.log('*** App starting ***');
       // console.log('thisApp:', thisApp);
-      console.log('classNames:', classNames);
-      console.log('settings:', settings);
+      // console.log('classNames:', classNames);
+      // console.log('settings:', settings);
       // console.log('templates:', templates);
 
       thisApp.initData();
