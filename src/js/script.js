1b77b9eed319c50b6c9a217476877e43b1581765
Kos Krzysztof <krzysztof.kos33@gmail.com>
2021-06-29 14:51:46 +0200

    Add fetch for app.initData method

--- src/js/script.js
+++ src/js/script.js
@@ -552,20 +552,39 @@
       const thisApp = this;
 
       for(let productData in thisApp.data.products) {
-        new Product(productData, thisApp.data.products[productData]);
+        new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
       }
     },
 
     initData: function() {
       const thisApp = this;
-      thisApp.data = dataSource;
+      const url = `${settings.db.url}/${settings.db.products}`;
+
+      thisApp.data = {};
+
+      fetch(url)
+        .then(function(rawResponse) {
+          console.log('first then rawResponse: ', rawResponse);
+          return rawResponse.json();
+        })
+        .then(function(parsedResponse) {
+          console.log('second then parsedResponse: ', parsedResponse);
+
+          // save parsedResponse as thisApp.data.products
+          thisApp.data.products = parsedResponse;
+
+          // execute initMenu method
+          thisApp.initMenu();
+        });
+
+      console.log('thisApp.data: ', JSON.stringify(thisApp.data));
+
     },
 
     init: function(){
       const thisApp = this;
 
       thisApp.initData();
-      thisApp.initMenu();
       thisApp.initCart();
     },
   };
